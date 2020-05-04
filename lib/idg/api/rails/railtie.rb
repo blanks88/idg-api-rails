fc597ebd94ac5c31da9d00850f956d73b233c41a
Manolo Martinez <rosso.neeri@gmail.com>
2020-05-04 17:48:27 -0500

    #feature register idg generator at railtie.rb

--- lib/idg/api/rails/railtie.rb
+++ lib/idg/api/rails/railtie.rb
@@ -2,6 +2,12 @@ module Idg
   module Api
     module Rails
       class Railtie < ::Rails::Railtie
+
+        # Register generators
+        config.app_generators do
+          require "#{ File.dirname __dir__ }/generators/idg/idg_generator.rb"
+        end
+
       end
     end
   end
