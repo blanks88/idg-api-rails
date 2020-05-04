e60afdb9acb059eda033589d4074cc5014a49b39
Manolo Martinez <rosso.neeri@gmail.com>
2020-05-04 17:22:41 -0500

    #feature idg generator definition with :delete service to test

---
+++ lib/generators/idg/idg_generator.rb
@@ -0,0 +1,9 @@
+class IdgGenerator < Rails::Generators::ScaffoldControllerGenerator
+  # source_root File.expand_path('templates', __dir__)
+
+
+  def create_delete_service_file
+    template 'delete_service.rb.tt', File.join("app/controllers", controller_class_path, "#{controller_file_name}_delete_service.rb")
+  end
+
+end
