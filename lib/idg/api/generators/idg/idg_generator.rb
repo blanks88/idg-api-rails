e25e69115e8f87ec6197841a3f25a50528b0c493
Manolo Martinez <rosso.neeri@gmail.com>
2020-05-05 10:06:24 -0500

    #setup idg_generator includes ResourceHelpers

--- lib/idg/api/generators/idg/idg_generator.rb
+++ lib/idg/api/generators/idg/idg_generator.rb
@@ -1,6 +1,9 @@
+require "rails/generators/resource_helpers"
 require 'rails/generators'
 
 class IdgGenerator < Rails::Generators::NamedBase
+  include Rails::Generators::ResourceHelpers
+
   source_root File.expand_path('templates', __dir__)
 
   def create_services_files
