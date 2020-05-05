06898f8f159843eb726640a955280274f58ed55f
Manolo Martinez <rosso.neeri@gmail.com>
2020-05-05 17:19:49 -0500

    #feature inherits from controller

--- lib/idg/api/generators/idg/idg_generator.rb
+++ lib/idg/api/generators/idg/idg_generator.rb
@@ -1,15 +1,21 @@
-require "rails/generators/resource_helpers"
 require 'rails/generators'
+require 'rails/generators/rails/scaffold_controller/scaffold_controller_generator'
 
 module Rails
   module Generators
-    class IdgGenerator < Rails::Generators::NamedBase
-      include Rails::Generators::ResourceHelpers
+    class IdgGenerator < Rails::Generators::ScaffoldControllerGenerator
+      idg_root File.expand_path('templates', __dir__)
 
-      source_root File.expand_path('templates', __dir__)
+      def create_controller_files
+        if options.api?
+          puts 'It`s not an Rails API, so only services generated'
+          super
+        else
+          template "#{idg_root}/api_controller.rb.erb", File.join("app/controllers", controller_class_path, "#{controller_file_name}_controller.rb")
+        end
 
-      def create_idg_file
-        template 'delete_service.rb.erb', File.join("app/controllers", controller_class_path, "#{controller_file_name}_delete_service.rb")
+
+        template "#{idg_root}/delete_service.rb.erb", File.join("app/services", controller_class_path, "#{controller_file_name}_delete_service.rb")
       end
 
     end
--- lib/idg/api/rails/version.rb
+++ lib/idg/api/rails/version.rb
@@ -1,7 +1,7 @@
 module Idg
   module Api
     module Rails
-      VERSION = '0.1.18'
+      VERSION = '0.1.19'
     end
   end
 end
