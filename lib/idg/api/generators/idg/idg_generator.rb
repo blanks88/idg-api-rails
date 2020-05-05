d81be4e1668dc1bcccc5805e1af6282cdd28ff51
Manolo Martinez <rosso.neeri@gmail.com>
2020-05-05 11:04:59 -0500

    #fix create function

--- lib/idg/api/generators/idg/idg_generator.rb
+++ lib/idg/api/generators/idg/idg_generator.rb
@@ -6,7 +6,7 @@ class IdgGenerator < Rails::Generators::NamedBase
 
   source_root File.expand_path('templates', __dir__)
 
-  def create_services_files
+  def create_idg_file
     template 'delete_service.rb.erb', File.join("app/controllers", controller_class_path, "#{controller_file_name}_delete_service.rb")
   end
 
