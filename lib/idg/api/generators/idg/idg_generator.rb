f2b6d5bbc40d8f3e6961ec25dde0398e0f285a6a
Manolo Martinez <rosso.neeri@gmail.com>
2020-05-05 17:34:54 -0500

    #fix source_root File.expand_path("../templates", __FILE__)

--- lib/idg/api/generators/idg/idg_generator.rb
+++ lib/idg/api/generators/idg/idg_generator.rb
@@ -4,11 +4,10 @@ require 'rails/generators/rails/scaffold_controller/scaffold_controller_generato
 module Rails
   module Generators
     class IdgGenerator < Rails::Generators::ScaffoldControllerGenerator
+      source_root File.expand_path("../templates", __FILE__)
 
       def create_controller_files
-        idg_root = File.expand_path('templates', __dir__)
-        puts idg_root
-
+        # Controller generator
         if options.api?
           puts 'It`s not an Rails API, so only services generated'
           super
@@ -16,8 +15,7 @@ module Rails
           template "api_controller.rb.erb", File.join("app/controllers", controller_class_path, "#{controller_file_name}_controller.rb")
         end
 
-
-        template "#{idg_root}/delete_service.rb.erb", File.join("app/services", controller_class_path, "#{controller_file_name}_delete_service.rb")
+        template "delete_service.rb.erb", File.join("app/services", controller_class_path, "#{controller_file_name}_delete_service.rb")
       end
 
     end
