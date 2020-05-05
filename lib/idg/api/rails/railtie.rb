cac589c0c09157c6c4856cf6a9ca031d3056b6a6
Manolo Martinez <rosso.neeri@gmail.com>
2020-05-05 16:31:46 -0500

    #fix cannot load file

--- lib/idg/api/rails/railtie.rb
+++ lib/idg/api/rails/railtie.rb
@@ -5,10 +5,8 @@ module Idg
 
         # Register generators
         config.app_generators do
-          # require "#{ File.dirname __dir__ }/generators/idg/idg_generator.rb"
-          # require "#{ File.dirname __dir__ }/rails/resource_override.rb"
-          require 'generators/idg/idg_generator.rb'
-          require 'generators/rails/resource_override'
+          require "#{ File.dirname __dir__ }/generators/idg/idg_generator.rb"
+          require "#{ File.dirname __dir__ }/generators/idg/resource_override.rb"
         end
 
       end
--- lib/idg/api/rails/version.rb
+++ lib/idg/api/rails/version.rb
@@ -1,7 +1,7 @@
 module Idg
   module Api
     module Rails
-      VERSION = '0.1.16'
+      VERSION = '0.1.17'
     end
   end
 end
