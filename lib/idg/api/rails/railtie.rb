faf7a3fb09bb0a36d42fd0a36a1df245c2bc2b08
Manolo Martinez <rosso.neeri@gmail.com>
2020-05-05 15:57:29 -0500

    #fix path file

--- lib/idg/api/rails/railtie.rb
+++ lib/idg/api/rails/railtie.rb
@@ -6,7 +6,7 @@ module Idg
         # Register generators
         config.app_generators do
           require "#{ File.dirname __dir__ }/generators/idg/idg_generator.rb"
-          require "#{ File.dirname __dir__ }/rails/resource_overrides.rb"
+          require "#{ File.dirname __dir__ }/rails/resource_override.rb"
         end
 
       end
