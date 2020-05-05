c79b7a0e93349c8dbf8646845898999cc70f4b50
Manolo Martinez <rosso.neeri@gmail.com>
2020-05-05 16:30:23 -0500

    #fix idg not found

--- lib/idg/api/generators/idg/idg_generator.rb
+++ lib/idg/api/generators/idg/idg_generator.rb
@@ -1,13 +1,17 @@
 require "rails/generators/resource_helpers"
 require 'rails/generators'
 
-class IdgGenerator < Rails::Generators::NamedBase
-  include Rails::Generators::ResourceHelpers
+module Rails
+  module Generators
+    class IdgGenerator < Rails::Generators::NamedBase
+      include Rails::Generators::ResourceHelpers
 
-  source_root File.expand_path('templates', __dir__)
+      source_root File.expand_path('templates', __dir__)
 
-  def create_idg_file
-    template 'delete_service.rb.erb', File.join("app/controllers", controller_class_path, "#{controller_file_name}_delete_service.rb")
+      def create_idg_file
+        template 'delete_service.rb.erb', File.join("app/controllers", controller_class_path, "#{controller_file_name}_delete_service.rb")
+      end
+
+    end
   end
-
 end
--- lib/idg/api/rails/railtie.rb
+++ lib/idg/api/rails/railtie.rb
@@ -5,8 +5,10 @@ module Idg
 
         # Register generators
         config.app_generators do
-          require "#{ File.dirname __dir__ }/generators/idg/idg_generator.rb"
-          require "#{ File.dirname __dir__ }/rails/resource_override.rb"
+          # require "#{ File.dirname __dir__ }/generators/idg/idg_generator.rb"
+          # require "#{ File.dirname __dir__ }/rails/resource_override.rb"
+          require 'generators/idg/idg_generator.rb'
+          require 'generators/rails/resource_override'
         end
 
       end
